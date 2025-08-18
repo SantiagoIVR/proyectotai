136f0daaada6701e677353fd6f1d8e66e4b57514
SantiagoIVR <santiago.izquierdo2203@uppuebla.edu.mx>
2025-08-17 22:28:32 -0600

    Video 13

--- src/pages/Reporteprendas.js
+++ src/pages/Reporteprendas.js
@@ -3,17 +3,53 @@ import Prenda from "../componentes/Prenda"
 import Tipoprenda from "../componentes/Tipoprenda"
 
 class Reporteprendas extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            datos:[
+                {
+                    "modelo":"Overzise",
+                    "foto":"fotos/playeraroja.jpeg",
+                    "color":"Roja",
+                    "talla":"M",
+                    "colorfondo":"Green"
+                },
+                {
+                    "modelo":"Slim",
+                    "foto":"fotos/playerarosa.jpeg",
+                    "color":"Rosa",
+                    "talla":"S",
+                    "colorfondo":"Blue"
+                },
+                {
+                    "modelo":"Slim Fit",
+                    "foto":"fotos/playeranegra.jpeg",
+                    "color":"Negra",
+                    "talla":"G",
+                    "colorfondo":"Red"
+                }
+            ]
+        }
+    }
     render(){
         return(<div>
-            <div><Tipoprenda
-                tipo="Overzise"/></div>
-                <div><Prenda
-                color="Roja"
-                talla="M"
-                escribe="Escribe algo..."
-                foto="fotos/playeraroja.jpeg"
-                colorfondo="Blue"/></div>
-        </div>)
+                <div>
+                    <Tipoprenda
+                    modelo="Overzise"/>
+                </div>
+                <div>
+                    {this.state.datos.map((reporte) => {
+                        return(
+                            <Prenda
+                            color={reporte.color}
+                            talla={reporte.talla}
+                            escribe="Escribe algo..."
+                            foto={reporte.foto}
+                            colorfondo={reporte.colorfondo}/>
+                        )
+                    })}
+                </div>
+            </div>)
     }
 }
 
